db55758066e62b849597c806a8530105b8a670db
Avraam Marimpis <makhsm@gmail.com>
2020-02-07 18:41:45 +0100

    Simple script to fetch the json files.

--- project/dataset/fetch.sh
+++ project/dataset/fetch.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 wget "https://www.mtgjson.com/json/THB.json"
+
+wget "https://www.mtgjson.com/json/ELD.json"
+
+wget "https://www.mtgjson.com/json/M20.json"
+
+wget "https://www.mtgjson.com/json/WAR.json"
