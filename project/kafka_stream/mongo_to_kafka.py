f93deb87d588d2975b27481979973fa9f4d31427
Avraam Marimpis <makhsm@gmail.com>
2020-02-13 15:41:27 +0100

    * Add simple call to clear up / delete the topic from the command line.
    * Stream some example data to Kafka.

--- project/kafka_stream/mongo_to_kafka.py
+++ project/kafka_stream/mongo_to_kafka.py
@@ -1,10 +1,15 @@
 from time import sleep
 from json import dumps
 import sys
+import os
 from pymongo import MongoClient
 from kafka import KafkaProducer
 
 if __name__ == "__main__":
+    os.system(
+        "/home/vagrant/opt/kafka/bin/kafka-topics.sh --zookeeper localhost:2181 --delete --topic tweets"
+    )
+
     mongo_client = MongoClient("localhost:27017")
     collection = mongo_client.mtg_tweets.all
 
@@ -13,11 +18,11 @@ if __name__ == "__main__":
         value_serializer=lambda x: dumps(x).encode("utf-8"),
     )
 
-    # topic = "numtest"
-    # for e in range(1000):
-    #     data = {"number": e}
-    #     producer.send(topic, value=data)
-    #     print("{0} {1}".format(topic, data))
-    #     sleep(5)
+    topic = "tweets"
+    for e in range(1000):
+        data = {"number": e}
+        # print("{0} {1}".format(topic, data))
+        producer.send(topic, value=data)
+        # sleep(5)
 
     print("Done.")
