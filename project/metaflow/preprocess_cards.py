dbcd0265babf5f9e3a0a38d5eb0a5099c4f5d69d
Avraam Marimpis <makhsm@gmail.com>
2020-04-27 09:31:09 +0200

    Add all cli-metaflow related applications here.

---
+++ project/metaflow/preprocess_cards.py
@@ -0,0 +1,61 @@
+# pylint: disable-all
+import json
+import numpy as np
+import os.path
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+from metaflow import FlowSpec, Parameter, step
+
+import config
+
+
+class PreprocessCards(FlowSpec):
+    keyruneCodes = Parameter("keyruneCodes", default="IKO,THB,ELD,M20,WAR")
+
+    @step
+    def start(self):
+        """ Begin here. """
+
+        self.list_keyruneCodes = list()
+
+        for code in self.keyruneCodes.split(","):
+            json_file = f"{config.DATASET}/{code}.json"
+
+            if os.path.exists(json_file):
+                self.list_keyruneCodes.append(code)
+
+        self.next(self.process_json, foreach="list_keyruneCodes")
+
+    @step
+    def process_json(self):
+        """ Read data. """
+        self.code = self.input
+
+        json_file = f"{config.DATASET}/{self.code}.json"
+        cards = None
+        with open(json_file, "r") as fp:
+            raw_data = json.load(fp)
+            cards = raw_data["cards"]
+        n = len(cards)
+
+        fname = f"{config.OUTPUT_DATASET}/{self.code}.json"
+        with open(fname, "w") as fp:
+            json.dump(cards, fp)
+
+        self.next(self.join)
+
+    @step
+    def join(self, inputs):
+        """Join our parallel branches."""
+
+        self.next(self.end)
+
+    @step
+    def end(self):
+        """ Finalize and clean up. """
+        print("All done.")
+
+
+if __name__ == "__main__":
+    # run preprocess_cards.py run --keyruneCodes 'THB,ELD'
+    PreprocessCards()
---
+++ project/metaflow/preprocess_fn.py
@@ -0,0 +1,97 @@
+import sys
+import pandas as pd
+import numpy as np
+import pyspark
+from pyspark.sql import SparkSession, DataFrame
+import pyspark.sql.functions as fn
+from pyspark.sql.functions import udf
+from pyspark.ml.feature import StringIndexer, IndexToString
+
+
+sys.path.append("../conf/")
+import config
+
+import preprocess_fn_text_rules
+
+text_rules = preprocess_fn_text_rules.text_rules
+
+
+def spark_session(spark=None) -> pyspark.sql.SparkSession:
+    if spark is None:
+        spark = SparkSession.builder.appName("mtggg").getOrCreate()
+    return spark
+
+
+def remove_duplicate_cards(df) -> pyspark.sql.DataFrame:
+    pd_names = df.select(["number", "name"]).toPandas()
+
+    unique_names, indices, counts = np.unique(
+        pd_names["name"], return_index=True, return_counts=True
+    )
+
+    pd_unique_names = pd_names.loc[indices]
+
+    df_filter = spark_session().createDataFrame(pd_unique_names)
+    df_filtered = df_filter.join(df, on="number", how="left").drop(df_filter.name)
+
+    return df_filtered
+
+
+def drop_columns(df) -> pyspark.sql.DataFrame:
+    keep_cols = [
+        "colorIdentity",
+        "convertedManaCost",
+        "colors",
+        "manaCost",
+        "name",
+        "number",
+        "text",
+        "power",
+        "rarity",
+        "subtypes",
+        "supertypes",
+        "toughness",
+        "types",
+    ]
+
+    remove_cols = list(set(df.columns) - set(keep_cols))
+
+    df_filtered = df.drop(*remove_cols)
+
+    return df_filtered
+
+
+@udf
+def udf_filter_text(name, text):
+    if isinstance(text, str):
+        new_text = text
+        new_text = new_text.replace(name, "CARDNAME")
+        for line in new_text:
+            for rule, replace in text_rules.items():
+                new_text = new_text.replace(rule, replace)
+
+        #         print(new_text)
+
+        return new_text
+
+
+def explode_to_strs(df, cols):
+    for col in cols:
+        df_edited = df.selectExpr(["number", col]).select(
+            "number", fn.expr(f"concat_ws(',', {col})").alias(f"str_{col}")
+        )
+        df = df.join(df_edited, on="number")
+    return df
+
+
+def encode_strings(df, cols):
+    for col in cols:
+        indexer = StringIndexer(
+            inputCol=f"{col}", outputCol=f"encoded_{col}", stringOrderType="alphabetAsc"
+        )
+        model = indexer.fit(df)
+        df = model.transform(df)
+
+        indexer.save(f"/tmp/pyspark/stringindexer_{col}")
+        model.save(f"/tmp/pyspark/stringindexer_model_{col}")
+    return df
