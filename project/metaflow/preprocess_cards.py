6f4feb310225fb5d4d6f78c7438d389df3941034
Avraam Marimpis <makhsm@gmail.com>
2020-05-16 23:26:25 +0200

    Rename class to BasicPreprocessing.

--- project/metaflow/preprocess_cards.py
+++ project/metaflow/preprocess_cards.py
@@ -6,47 +6,12 @@ from metaflow import FlowSpec, Parameter, step
 import config
 
 
-class PreprocessCards(FlowSpec):
-    keyruneCodes = Parameter("keyruneCodes", default="IKO,THB,ELD,M20,WAR")
-
+class BasicPreprocessing(FlowSpec):
     @step
     def start(self):
         """ Begin here. """
 
-        self.list_keyruneCodes = list()
-
-        for code in self.keyruneCodes.split(","):
-            json_file = f"{config.DATASET}/{code}.json"
-
-            if os.path.exists(json_file):
-                self.list_keyruneCodes.append(code)
-
-        self.next(self.process_json, foreach="list_keyruneCodes")
-
-    @step
-    def process_json(self):
-        """ Read data. """
-
-        self.code = self.input
-
-        json_file = f"{config.DATASET}/{self.code}.json"
-        cards = None
-        with open(json_file, "r") as fp:
-            raw_data = json.load(fp)
-            cards = raw_data["cards"]
-        n = len(cards)
-
-        fname = f"{config.OUTPUT_DATASET}/{self.code}.json"
-        with open(fname, "w") as fp:
-            json.dump(cards, fp)
-
-        self.next(self.join)
-
-    @step
-    def join(self, inputs):
-        """Join our parallel branches."""
-
-        self.next(self.end)
+        pass
 
     @step
     def end(self):
@@ -55,5 +20,46 @@ class PreprocessCards(FlowSpec):
         print("All done.")
 
 
-if __name__ == "__main__":
-    PreprocessCards()
+#     @step
+#     def start(self):
+#         """ Begin here. """
+#
+#         self.list_keyruneCodes = list()
+#
+#         for code in self.keyruneCodes.split(","):
+#             json_file = f"{config.DATASET}/{code}.json"
+#
+#             if os.path.exists(json_file):
+#                 self.list_keyruneCodes.append(code)
+#
+#         self.next(self.process_json, foreach="list_keyruneCodes")
+#
+#     @step
+#     def process_json(self):
+#         """ Read data. """
+#
+#         self.code = self.input
+#
+#         json_file = f"{config.DATASET}/{self.code}.json"
+#         cards = None
+#         with open(json_file, "r") as fp:
+#             raw_data = json.load(fp)
+#             cards = raw_data["cards"]
+#         n = len(cards)
+#
+#         fname = f"{config.OUTPUT_DATASET}/{self.code}.json"
+#         with open(fname, "w") as fp:
+#             json.dump(cards, fp)
+#
+#         self.next(self.join)
+#
+#     @step
+#     def join(self, inputs):
+#         """Join our parallel branches."""
+#
+#         self.next(self.end)
+#
+#
+#
+# if __name__ == "__main__":
+#     PreprocessCards()
