fe5f004434d165c2162869bdd6427d03365a1a14
Avraam Marimpis <makhsm@gmail.com>
2020-05-16 23:24:24 +0200

    * Add TEMP directory.
    * Add Metaflow directory.

--- project/config/config.py
+++ project/config/config.py
@@ -3,6 +3,8 @@ import os
 ROOT = "/mtgp/"  # os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FLASK = os.path.abspath(os.path.join(ROOT, "web"))
 DATASET = os.path.abspath(os.path.join(ROOT, "dataset"))
+METAFLOW = os.path.abspath(os.path.join(ROOT, "metaflow"))
+TEMP = "/tmp/"
 
 ARTIFACTS = os.path.abspath(os.path.join(ROOT, "artifacts"))
 OUTPUT_DATASET = os.path.abspath(os.path.join(ARTIFACTS, "dataset"))
