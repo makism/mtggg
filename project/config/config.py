ffe0e11c33ceceb2ac386982e37886bc3ff84444
Avraam Marimpis <makhsm@gmail.com>
2020-07-30 10:54:39 +0200

    Add configuration variables for airflow, ml and saved images directories.

--- project/config/config.py
+++ project/config/config.py
@@ -8,6 +8,11 @@ TEMP = "/tmp/"
 
 ARTIFACTS = os.path.abspath(os.path.join(ROOT, "artifacts"))
 OUTPUT_DATASET = os.path.abspath(os.path.join(ARTIFACTS, "dataset"))
+SCRYFALL_IMAGES = os.path.abspath(os.path.join(ARTIFACTS, "scryfall_images"))
+ML_MODELS = os.path.abspath(os.path.join(ARTIFACTS, "ml"))
 
 DB_VERSIONING = os.path.abspath(os.path.join(ARTIFACTS, "versioning.db"))
 SPARK_MODELS = os.path.abspath(os.path.join(ARTIFACTS, "spark_models"))
+
+AIRFLOW_PROJECT_DIR = os.path.abspath(os.path.join(ROOT, "airflow"))
+AIRFLOW_DAGS_DIR = "/home/vagrant/airflow/dags"
